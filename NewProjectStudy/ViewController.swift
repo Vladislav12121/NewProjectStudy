dd3f1b71880cc62fc1105b82d13709924fa18e25
Vladislav12121 <petr223333@mail.ru>
2023-06-17 20:49:43 +0600

    Initial Commit

---
+++ NewProjectStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewProjectStudy
+//
+//  Created by Владислав Клунный on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
